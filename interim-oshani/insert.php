0cafce6887fe16a108026a06ca6b69f4babbd0aa
oshaniwickramasinghe <84929391+oshaniwickramasinghe@users.noreply.github.com>
2022-11-21 16:30:54 +0530

    updated

--- interim-oshani/insert.php
+++ interim-oshani/insert.php
@@ -15,7 +15,6 @@
 		$last_name = $_REQUEST['last_name'];
 		$email = $_REQUEST['email'];
 		$contactNo = $_REQUEST['contactNo'];
-<<<<<<< HEAD
 		$password = md5($_POST['password']);
 		$cpassword = md5($_POST['cpassword']);
 		$role = $_REQUEST['role'];
