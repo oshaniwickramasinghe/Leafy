7657f56c31f2944b3fd3baeb2b146bbaff588ccf
oshaniwickramasinghe <84929391+oshaniwickramasinghe@users.noreply.github.com>
2022-11-21 09:19:42 +0530

    check encrypt

--- interim-oshani/insert.php
+++ interim-oshani/insert.php
@@ -15,10 +15,10 @@
 		$last_name = $_REQUEST['last_name'];
 		$email = $_REQUEST['email'];
 		$contactNo = $_REQUEST['contactNo'];
-		//$passward = md5($_REQUEST['passward']);
-		//$conpassward = md5($_REQUEST['conpassward']);
-		$passward = $_REQUEST['passward'];
-		$conpassward = $_REQUEST['conpassward'];
+		$passward = md5($_POST['passward']);
+		$conpassward = md5($_POST['conpassward']);
+		//$passward = $_REQUEST['passward'];
+		//$conpassward = $_REQUEST['conpassward'];
 		$role = $_REQUEST['role'];
 		
 		
