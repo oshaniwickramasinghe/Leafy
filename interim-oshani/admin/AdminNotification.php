8f122a353a7110d5581cd257980651c13bffcaf1
oshaniwickramasinghe <84929391+oshaniwickramasinghe@users.noreply.github.com>
2022-11-29 09:45:25 +0530

    notification

---
+++ interim-oshani/admin/AdminNotification.php
@@ -0,0 +1,69 @@
+<html>  
+<head>  
+    <title>Admin Notification</title>  
+    <link rel = "stylesheet" type = "text/css" href ='home.css'>   
+</head>  
+
+<body>  
+
+<?php include 'home.php';?>
+
+    <div class="div1">
+        <div class="div2">
+        <h2>some text</h2>
+            <h2>some words</h2>
+        </div>
+        <div class="div2">
+        <h2>some text</h2>
+            <h2>some words</h2>
+        </div>
+        
+        <div class="div2">
+        <h2>some text</h2>
+            <h2>some words</h2>
+        </div>
+        <div class="div2">
+        <h2>some text</h2>
+            <h2>some words</h2>
+        </div>
+        <div class="div2">
+        <h2>some text</h2>
+            <h2>some words</h2>
+        </div>
+        <div class="div2">
+        <h2>some text</h2>
+            <h2>some words</h2>
+        </div>
+        <div class="div2">
+        <h2>some text</h2>
+            <h2>some words</h2>
+        </div>
+        <div class="div2">
+        <h2>some text</h2>
+            <h2>some words</h2>
+        </div>
+        
+        <div class="div2">
+        <h2>some text</h2>
+            <h2>some words</h2>
+        </div>
+        <div class="div2">
+        <h2>some text</h2>
+            <h2>some words</h2>
+        </div>
+        <div class="div2">
+        <h2>some text</h2>
+            <h2>some words</h2>
+        </div>
+        <div class="div2">
+        <h2>some text</h2>
+            <h2>some words</h2>
+        </div>
+            
+
+    </div>
+
+    
+    
+</body>     
+</html>  
