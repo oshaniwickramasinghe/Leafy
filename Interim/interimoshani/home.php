7e5b98c60aff2530a4ff3b582e65b04985dc8214
govisahas <88455215+govisahas@users.noreply.github.com>
2022-12-03 20:58:27 +0530

    Add files via upload

---
+++ Interim/interimoshani/home.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+?>
+<html>  
+    <head>  
+        <title>Home</title>  
+        <link rel = "stylesheet" type = "text/css" href = "style.css">   
+    </head> 
+
+    <body>  
+
+    <div class="header">
+        <h1>Leafy</h1>   
+    </div>
+
+        <div align='center'>
+            <p>Click here to register a user</p>
+            <a href="form.php">
+                <button id="button">form</button>
+            </a>
+        
+            <p>Click here view users</p>
+            <a href="view.php">
+                <button id="button">view</button>
+            </a>
+
+            <p>Click here to add an item</p>
+            <a href="item.html">
+                <button id="button">Item</button>
+            </a>
+        </div>  
+
+    <div class="footer">
+        <p>Click here to logout
+            <a href="logout.php">
+                <button id="logout">Logout</button>
+            </a>
+        </p>
+    </div>
+        
+    </body>     
+</html>  
