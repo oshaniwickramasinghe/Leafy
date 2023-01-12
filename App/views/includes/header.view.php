1f474c5f8ba28d1ce0c23653db7bcbc36194ea1f
govisahas <88455215+govisahas@users.noreply.github.com>
2023-01-12 19:56:00 +0530

    Update header.view.php

--- App/views/includes/header.view.php
+++ App/views/includes/header.view.php
@@ -1,11 +1,14 @@
 <html>
 <link rel="stylesheet" href="../public/assets/css/curve.css">
+<title>
+    <?=ucfirst(App::$page)?>
+</title>   
 <header>
 
 <div class="header">
         <div class="center_wrapper">
             <div class="left_part">
-                <div class="logo">
+                <div class="logo_1">
                     <a href="">
                         <img src="../public/assets/images/logo.svg" alt="">
                     </a>
@@ -29,8 +32,16 @@
             </div>
             
             <div class="right_part">
-            <ul>   
-            <li><a href="login" class="">Login</a></li>
+            <ul> 
+            <?php if(!Auth::logged_in()):?>  
+               <li><a href="login" class="">Login</a></li>
+            <?php else: ?>
+                <div>
+                <li><a href="#" class="">Hi,<?=Auth::getfname()?></a></li>
+                    
+                </div>
+                <li><a href="logout" class="">Logout</a></li>
+            <?php endif; ?>
             <li><a href="" class="">Languages (EN)</a></li>
                 
            </ul>
