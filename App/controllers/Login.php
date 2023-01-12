60bd511189fda6f230342526730d0d2e1c7b78da
govisahas <88455215+govisahas@users.noreply.github.com>
2023-01-12 19:47:26 +0530

    Update Login.php

--- App/controllers/Login.php
+++ App/controllers/Login.php
@@ -8,10 +8,37 @@ class Login extends Controller{
    public  function index(){
 
  
-     
+    $data['errors'] = [];
+
     $data['title'] = "login";
+    $user = new User();
+
+    if($_SERVER['REQUEST_METHOD'] == "POST") 
+    {
+        //validate
+
+//check for th email
+
+            $row = $user->first(array(
+            'email' =>$_POST['email']));
+        
+        if($row)
+           {
+            
+               if(password_verify($_POST['password'],$row->password ))
+               {
+                 //authenticate
+                 //
+                 Auth::authentication($row);
+                 redirect('customerhome');
+
+               }
+           }
+           $data['errors']['email'] = "Wrong email or password";
+    
+}
         $this->view('login', $data);
     }
-
+  
   
 }
