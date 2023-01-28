1de073c0eab03564ee88f95410282d62174c9ce7
oshaniwickramasinghe <84929391+oshaniwickramasinghe@users.noreply.github.com>
2023-01-28 23:55:47 +0530

    reports

--- admin/AdminReport.php
+++ admin/AdminReport.php
@@ -1,68 +1,115 @@
-<?php
+<?php 
+    $username= "student";
+    $password= "student";
+    $database = "leafy";
 
-include 'connect.php';
+    try{
+        $pdo = new PDO("mysql:host=localhost;database=$database",$username,$password);
 
-    $sql ="SELECT * FROM users";
-    $result = mysqli_query($conn,$sql);
-    $chart_data="";
-    while ($row = mysqli_fetch_array($result)) { 
-
-    $firstname=$row['first_name'];
-    $id[] = $row['user_id'];
-}
- 
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch(PDOException $e){
+        die("Error: not connected.".$e->getMessage());
+    }
 ?>
 
+<?php 
+
+include '../includes/header.view.php';
+
+
+?>
 
 <!DOCTYPE html>
-<html lang="en"> 
+<html lang="=en">
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Graph</title> 
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width", initial-scale="1">
+        <title>chart.js</title>
+        <style type="text/css">
+            .chartBox{
+                width: 700px;
+            }
+        
+        </style>
     </head>
+
     <body>
-        <div style="width:50%;height:20%;text-align:center">
-            <h2 class="page-header" >Analytics Sales Report </h2>
-            <div><?php echo $firstname; ?> </div>
-            <canvas  id="chartjs_line"></canvas> 
-        </div>    
-    </body>
-  <script src="//code.jquery.com/jquery-1.9.1.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
-    <script type="text/javascript">
-      var ctx = document.getElementById("chartjs_line").getContext('2d');
-                var myChart = new Chart(ctx, {
-                    type: 'line',
-                    data: {
-                        labels:<?php echo json_encode($month); ?>,
-                        datasets: [{
-                            backgroundColor: [
-                               "#5969ff",
-                                "#ff407b",
-                                "#25d5f2",
-                                "#ffc750",
-                                "#2ec551",
-                                "#7040fa",
-                                "#ff004e"
-                            ],
-                            data:<?php echo json_encode($sales); ?>,
-                        }]
-                    },
-                    options: {
-                           legend: {
-                        display: true,
-                        position: 'bottom',
- 
-                        labels: {
-                            fontColor: '#71748d',
-                            fontFamily: 'Circular Std Book',
-                            fontSize: 14,
-                        }
-                    },
- 
- 
+
+    <?php include '../includes/menu.view.php'?>
+        <?php
+            try{
+
+                $sql = "SELECT * FROM leafy.user_count ";
+                $result = $pdo->query($sql);
+                if($result->rowCount()>0){
+                    
+                     $count = array();
+                    
+                    while($row=$result->fetch()){
+                        $count[]=$row["count"];
+                        
+                        echo $row["count"];
+                    }
+                    unset($result);
                 }
-                });
-    </script>
+                else{
+                    echo "no records found";
+                }
+            }catch(PDOException $e){
+                die ("Error: unable to excecute $sql.".$e->getMessage());
+            }
+            unset($pdo);
+
+            
+        ?>
+
+        <div class="chartBox">
+        <canvas id="myChart"></canvas>
+        </div>
+        
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+        <script>
+
+            //setup block
+            const count = <?php echo json_encode($count);?>;
+           
+
+            const data = { 
+            labels: ['customer', 'agriculturalist', 'instructor', 'delivery_person'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: count,
+                    backgroundColor: [
+                        'rgba(54, 162, 235, 0.2)',
+                        
+                    ],
+                    borderColor: [
+                        'rgba(54, 162, 235, 1)',
+                        
+                    ],
+                    borderWidth: 1
+                }]
+            };
+            //config block
+            const config ={
+                    type: 'bar',
+                data,
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            };
+            //render block
+            const myChart = new Chart(
+                document.getElementById('myChart'),
+                config
+            );
+            
+        </script>
+    </body>
+
 </html>
