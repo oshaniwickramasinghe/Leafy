ee44b47e15c51831aef4e09d2d6d2f1542626b7d
Suresh1129 <2020cs026stu.ucsc.cmb.ac.lk>
2023-01-31 15:56:37 +0530

    new css

--- public/includes/header.view.php
+++ public/includes/header.view.php
@@ -1,43 +1,19 @@
-<<<<<<< HEAD:includes/header.view.php
-
 <html>
-<link rel="stylesheet" href="CSS/style.css">
-
-=======
->>>>>>> 5513fdc1da9bd97508fa236930afc30427686be7:public/includes/header.view.php
-<?php
-
-?>
-
-<html>
- 
-<<<<<<< HEAD:includes/header.view.php
-
-=======
->>>>>>> 5513fdc1da9bd97508fa236930afc30427686be7:public/includes/header.view.php
+<link rel="stylesheet" href="../CSS/style.css">
 <header>
 
 <div class="header">
         <div class="center_wrapper">
             <div class="left_part">
-<<<<<<< HEAD:includes/header.view.php
-
                 <div class="logo">
                     <a href="">
-                        <img src="../public/assets/images/logo.svg" alt="">
+                        <img src="../public/images/logo.svg" alt="">
 
                 <div class="logo_1">
                     <a href="">
                         <img src="../Customer/images/logo.svg" alt="">
 
                     </a>
-=======
-                <div class="logo_1">
-                    
-                        <img src="/images/logo.svg"  height= "121.42px" >
-                      
-                   
->>>>>>> 5513fdc1da9bd97508fa236930afc30427686be7:public/includes/header.view.php
                 </div>
                 <div class="menu">
                     <ul>
@@ -58,13 +34,9 @@
             </div>
             
             <div class="right_part">
-<<<<<<< HEAD:includes/header.view.php
-
             <ul>   
             <li><a href="login" class="">Login</a></li>
 
-=======
->>>>>>> 5513fdc1da9bd97508fa236930afc30427686be7:public/includes/header.view.php
             <ul> 
             <?php if(!logged_in()):
 
@@ -77,10 +49,7 @@
                 </div>
                 <li><a href="Logout.php" class="">Logout</a></li>
             <?php endif; ?>
-<<<<<<< HEAD:includes/header.view.php
 
-=======
->>>>>>> 5513fdc1da9bd97508fa236930afc30427686be7:public/includes/header.view.php
             <li><a href="" class="">Languages (EN)</a></li>
                 
            </ul>
