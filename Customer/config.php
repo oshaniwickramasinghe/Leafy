c6f37b5cf96894d4d172f104203c47ec8200829c
oshaniwickramasinghe <84929391+oshaniwickramasinghe@users.noreply.github.com>
2023-02-07 11:00:15 +0530

    may conflict with CSS

--- Customer/config.php
+++ Customer/config.php
@@ -9,8 +9,8 @@ if($_SERVER['SERVER_NAME'] == 'localhost'){
     //database config for local server
     define('DBHOST' , 'localhost');
     define('DBNAME' , 'leafy');
-    define('DBUSER' , 'root');
-    define('DBPASS' , '');
+    define('DBUSER' , 'student');
+    define('DBPASS' , 'student');
     define('DBDRIVER' , 'mysql');
 }else{
     //database config for live server
