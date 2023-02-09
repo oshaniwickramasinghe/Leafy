c6ecd2c1a27cb9f25187b8fcd8a7ed404fcacf6c
oshaniwickramasinghe <84929391+oshaniwickramasinghe@users.noreply.github.com>
2023-02-09 22:14:32 +0530

    Move profile up

--- admin/AdminUserView.php
+++ admin/AdminUserView.php
@@ -74,7 +74,7 @@ $resultcustomer2= mysqli_query($conn,$sqlcustomer2);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../public/CSS/style.css"> 
     <!-- <link rel="stylesheet" href="profile.css"> -->
-    <title>Instructor Home</title>
+    <title>User Details</title>
     
     
 </head>
@@ -89,6 +89,7 @@ $resultcustomer2= mysqli_query($conn,$sqlcustomer2);
     <!-- <div class = "home_body">  -->
     <!-- <div class="container"> -->
 
+    <div class="top">
         
     <div class="profile">
             <div class="profile-text">
@@ -149,6 +150,7 @@ $resultcustomer2= mysqli_query($conn,$sqlcustomer2);
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     
 
