57e32489c1bc9c11fea526845c349b37e14444b4
Suresh1129 <2020cs026stu.ucsc.cmb.ac.lk>
2023-02-10 01:44:01 +0530

    contact page

---
+++ agriculturalist/contact.php
@@ -0,0 +1,39 @@
+<?php
+
+include '../public/Auth.php';
+include '../public/includes/header.view.php';
+// include 'database.php';
+?>
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="agristyle.css">
+
+
+<head>
+
+</head>
+
+<body>
+<div class="cont_hed" >
+    <p>Contact US</p>
+
+    <p>Any questions or remarks? Please Contact Us</p>
+
+</div>
+    <div class="cont_container">
+       
+        <p class="hed">Contact Information</p>
+        <div class="contact-info">
+            <p>Phone: 011-5556668</p>
+            <p>Facebook: <a href="">Leafy</a></p>
+            <p>Intagram: <a href="">Leafy</a></p>
+            <p>Email: <a href="">leafy@example.com</a></p>
+        </div>
+    </div>
+    <footer>
+<img src = "../Customer/images/Footer.svg"  height= "121.3px"  style = "">
+</footer>
+</body>
+
+</html>
+
