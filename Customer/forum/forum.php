05f335b3b32a8dc8f59ac56c780177b0799ca848
govisahas <88455215+govisahas@users.noreply.github.com>
2023-05-01 18:09:36 +0530

    Update forum.php

--- Customer/forum/forum.php
+++ Customer/forum/forum.php
@@ -1,6 +1,8 @@
+<link rel="stylesheet" href="../CSS/delivery.css">
+<link rel="stylesheet" href="../CSS/style.css">
 <?php
-include "../Auth.php";
-include "../database.php";
+include "../login/Auth.php";
+include "../database/database.php";
 include '../includes/header.php';
 
 
@@ -39,7 +41,7 @@ $row  = mysqli_fetch_array($result);
             <ul>
                 <li><a href="../customerhome.php"><i class="fa-solid fa-house"  style="font-size:16px;color:black;"></i>Home</a></li>
                 <li><a href="../wishlist/wishlist.php"><i class="fa fa-list" aria-hidden="true" style="font-size:16px;color:black;"></i>Wishlist</a></li>
-                <li><a href="../notification/notification.php"><i  class="fa fa-bell" aria-hidden="true"style="font-size:16px;color:black;"></i>Notifications <div class  = "count" style = "margin-top:-1%"><?php echo $row[0]?></div></a></li>
+                <li><a href="../notification/notification.php"><i  class="fa fa-bell" aria-hidden="true"style="font-size:16px;color:black;"></i>Notifications <div class  = "count" style = "margin-top:-13%"><?php echo $row[0]?></div></a></li>
                 <li><a href="../forum/forum.php"><i class="fa-solid fa-comments"  style="font-size:16px;color:black;"></i>Forum</a></li>
                 <li><a href="../history/history.php"><i class="fa-solid fa-gauge-high"  style="font-size:16px;color:black;"></i>History</a></li>
                 <li><a href="../location/location.php"><i class="fa-solid fa-location-arrow"  style="font-size:16px;color:black;"></i>Location</a></li>
@@ -51,18 +53,18 @@ $row  = mysqli_fetch_array($result);
 <div  class  =  "forum_body">
 
 
-
 <div  class  = "R">
 <div id="id01" class="modal" style="display: none;">
 <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
 <div id="ReplyModal" class="modal fade" role="dialog">
         <h4 class="modal-title">Reply Question</h4>
       </div>
-      <div class="modal-body">
+      <div class="modal-body" style=" margin-left: 12%">
+
         <form name="frm1" method="post">
             <input type="hidden" id="commentid" name="Rcommentid">
             <div class="form-group">
-              <label for="comment">Write your reply:</label>
+              <label for="comment">Write your reply:</label><br>
               <textarea class="form-control" rows="5" name="Rmsg" required></textarea>
             </div>
 <!-- after click on reply button modal will be hidden -->
