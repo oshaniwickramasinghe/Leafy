2c9c3a82db38b706c6f0df62407df10d6ffcff3d
Suresh1129 <91408704+Suresh1129@users.noreply.github.com>
2023-05-10 11:36:30 +0530

    final update

---
+++ agriculturalist/createpost_menu.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+   
+    <link rel="stylesheet" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+          <link rel="stylesheet" href="../Customer/CSS/style.css">
+</head>
+<body>
+
+
+ <div class="containerr">
+        <div class="left_menu_bar">
+            <div id="menu">
+                <a><i class="fa-solid fa-bars"></i></a>
+                <div class="image"><img src="images/badge.svg" alt=""></div>
+                <h3>Leafy</h3>
+            </div>
+            <ul>
+            
+                <li><a href="landing.php"><i class="fa-solid fa-house"  style="font-size:16px;color:black;"></i>Home</a></li>
+                <li><a href="agricreatepost.php"><i class="fa fa-list" aria-hidden="true" style="font-size:16px;color:black;"></i>Create Post</a></li>
+                <li><a href="postview.php"><i class="fa fa-list" aria-hidden="true" style="font-size:16px;color:black;"></i>My Posts</a></li>
+                <li><a href="notification/agriculturalnotification.php"><i  class="fa fa-bell" aria-hidden="true"style="font-size:16px;color:black;"></i>Orders</a></li>
+                <li><a href="agriculturalnotification.php"><i  class="fa fa-bell" aria-hidden="true"style="font-size:16px;color:black;"></i>Notifications</a></li>
+                <!-- <li><a href="agriD.php"><i class="fa-solid fa-comments"  style="font-size:16px;color:black;"></i>Report</a></li> -->
+                <li><a href="blog.php"><i class="fa-solid fa-gauge-high"  style="font-size:16px;color:black;"></i>Form</a></li>
+                
+            </ul>
+
+        </div>
+</div>
+
+
+</body>
+
+
+</html>
---
+++ agriculturalist/include/header.php
@@ -0,0 +1,140 @@
+<?php
+include "../database.php";
+?>
+
+<html>
+<link rel="stylesheet" href="../Customer/CSS/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" 
+ integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" 
+ referrerpolicy="no-referrer" />
+
+<header>
+<div class="header">
+
+        <div class="center_wrapper">
+            <div class="left_part">
+                <div class="logo_1">
+                        <img src="../photos/logo.svg"  height= "95.42px "  >
+
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li>
+                            <a href="../customerhome.php" class="">Home</a>
+                        </li>
+                        <li>
+                            <a href="#blogs" class="">Blogs</a>
+                        </li>
+                        <li>
+                            <a href="#courses" class="">Courses</a>
+                        </li>
+                        <li>
+                            <a href="../contact.php" class="">Contact</a>
+                        </li>
+                    </ul> 
+                </div>
+            </div>
+            
+            <div class="right_part">
+            <ul> 
+           
+
+               <div class="profile_icon">
+                    <div  class="user-pic" >
+                        <div class="user_details">
+                            
+                            <?php if(logged_in()){
+$user_ID = $_SESSION['USER_DATA']['user_id'];
+// if(!isset($user_ID)){
+//     header('location:login.php');
+// };
+
+// if(isset($_GET['logout'])){
+//      unset($user_ID);
+//      session_destroy();
+//      header('location:../Customer/home.view.php');
+// }
+
+
+$select=mysqli_query($conn,"SELECT * FROM user WHERE user_ID='$user_ID'") or die('query failed');
+
+if(mysqli_num_rows($select)>0){
+    $fetch= mysqli_fetch_assoc($select);
+}
+?>  
+                            <img src="../photos/profilepic_icon.svg"  >
+                            <p><?php echo $fetch['fname']; ?></p>
+                        </div>
+                        <button onclick="toggleMenu()">
+                        <span class="fa-solid fa-circle-chevron-down"  ></span>
+                        </button>
+                    </div>
+                </div>
+                <div class="sub-menu-wrap" id="subMenu">
+                    <div class="sub-menu">
+                        <div class="user-info">
+                            <img src="../photos/profilepic_icon.svg" alt=""  height= "100.42px">
+                            <p><?php echo $fetch['fname']; ?></p>
+                        </div>
+                        <hr>
+                        <a href = "../../instructor/InstructorHome.php" class="sub-menu-link">
+                            <i class="fa-solid fa-circle-user" style="font-size:16px;color:#43562B;"></i>
+                            <p>My Profile</p>
+                            <span>></span>
+                        </a>
+                        <a href = "../Logout.php" class="sub-menu-link">
+                            <i class="fa-solid fa-right-from-bracket" style="font-size:16px;color:#43562B;"></i>
+                            <p>Logout</p>
+                            <span>></span>
+                        </a>
+
+                    </div>
+
+                </div> 
+                <div class  = "lan">
+                <div class="translate">
+            <div id="google_translate_element"></div></div>
+<script type="text/javascript">
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en' , includedLanguages : 'si,en,EN'}, 'google_translate_element');
+  }
+</script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+            </div>
+
+            <?php }else{ ?>
+
+                <div class="login">
+                <li><a href="../../Customer/login.php">Login</a></li>
+                </div>
+                <div class  = "language">
+            <a href="" class="">Languages (EN)</a>
+            </div>
+
+            <?php } ?>
+
+            <!-- <li><a href="../Customer/Login.view.php" class="">Login</a></li> -->
+
+           </ul>
+
+           </div>
+        </div>          
+</div>
+ </header>
+
+
+<script>
+        
+        function toggleMenu()
+        {
+            var subMenu = document.getElementById("subMenu");
+            if(subMenu.style.display == "none")
+            {
+                subMenu.style.display = "block";
+            }else{
+                subMenu.style.display = "none";
+            }
+        }
+    </script>
+<html>
