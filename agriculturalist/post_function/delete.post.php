d45b95e246c3727bb399df25ae44d9a0ab274e8d
Suresh1129 <91408704+Suresh1129@users.noreply.github.com>
2023-05-12 01:28:48 +0530

    update 12

---
+++ agriculturalist/post_function/delete.post.php
@@ -0,0 +1,17 @@
+<?php
+include "../../public/Auth.php";
+include "../database.php";
+
+$postid=$_REQUEST['postid']; 
+
+// sql to delete a record
+$sql = "DELETE FROM post WHERE post_id=$postid";
+
+if ($conn->query($sql) === TRUE) {
+//   echo "<script> alert('Add Removed Successfully'); window.location.href='../account.php'; </script>";
+header("Location:  ../createpost/postview.php");
+} else {
+  echo "Error deleting record: " . $conn->error;
+}
+
+?>
