0fc7ef339c29c9f205372f575085a84dce688ef5
Suresh1129 <91408704+Suresh1129@users.noreply.github.com>
2023-05-12 02:51:00 +0530

    update 12

---
+++ agriculturalist/createpost/postview.php
@@ -0,0 +1,99 @@
+<?php 
+include '../Auth.php';
+include '../include/header.php';
+// include '../database.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>postview</title>
+  <!-- <link rel="stylesheet" href="../agristyle.css"> -->
+  <link rel="stylesheet" href="../CSS/delivery.css">
+  <link rel="stylesheet" href="../agristyle.css">
+  <link rel="stylesheet" href="../CSS/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />                                                   
+
+</head>
+
+<body>
+
+<?php 
+include "../include/createpost_menu.php";
+
+
+?>
+
+
+<div class = "vegetable_body">
+<div class = "row">
+
+<?php
+
+    $user_id =$_SESSION["USER_DATA"]["user_id"];
+   
+
+    $sql  =  "SELECT * FROM post WHERE user_id='$user_id' ";
+    $result = mysqli_query($conn , $sql);
+
+      // $query = "SELECT * FROM post ORDER BY post_id ASC";
+      // //execute mysql query and store data in result
+      // $result = mysqli_query($conn, $query);
+
+      if (mysqli_num_rows($result) > 0) {
+
+        while ($res = mysqli_fetch_array($result)) {
+      ?>
+
+
+
+<div class = "column_2" >
+<form method  = "Post " action  =  "postview.php"  enctype="multipart/form-data">  
+                <div class = "cards" >
+                    <div class = "card_body">
+
+                    <img src="../images/<?php echo $res['image'];?>" width="180" height="180" > 
+                    <h5 style="font-size:large;"><?php echo $res['item_name']?></h5>
+
+                    <h5 class = "text_danger">Location:   <?php echo $res['location'];?></h5>       
+                    <h5 class = "text_danger">Price: Rs  <?php echo $res['unit_price'];?>.00  per Kg</h5>
+                    <h5>Quantity :   <?php echo $res['quantity']?>kg </h5>
+                    <h5>Minimum Quantity :   <?php echo $res['minimum_quantity']?>kg </h5>
+                    <h5>Expire Date :   <?php echo $res['expire_date']?></h5>
+                    
+                    <div  class="edit_delete">
+                    <a   href="eddit.php?Qtt=<?php echo $res['quantity'] ?>&mQtt=<?php echo $res['minimum_quantity'] ?>
+                    &ex=<?php echo $res['expire_date'] ?>&price=<?php echo $res['unit_price'] ?>&loc=<?php echo $res['location'] ?>
+                    &img=<?php echo $res['image'] ?>&vegi=<?php echo $res['item_name'] ?>&postid=<?php echo $res['post_id'] ?>">Edit</a>
+
+                      <a  href="../post_function/delete.post.php?postid=<?php echo $res['post_id'] ?>">Delete</a>
+                      </div>
+                     <!-- <input type= "submit" name= "Delete" class= "btn" value= "Delete" data-inline = "true"/> 
+                     
+                     <input type= "submit" name= "Delete" class= "btn" value= "Delete" data-inline = "true"/>  -->
+
+                    
+                  </div>
+                </div>
+             </form> 
+       
+        </div>
+  <?php
+ 
+  }
+  }
+  ?>
+</div> 
+<footer>
+<?php include '../include/footer.php' ?>
+</footer>
+
+</body>
+
+</html>
+
---
+++ agriculturalist/landing.php
@@ -0,0 +1,54 @@
+<?php 
+
+include '../public/Auth.php';
+include 'include/header.view.php';
+
+// include 'database.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="agristyle.css">
+    <link rel="stylesheet" href="CSS/delivery.css">
+    <link rel="stylesheet" href="CSS/style.css">
+
+    <title>Agriculturalist Home</title>
+</head>
+<body>
+<?php include "createpost_menu.php" ?>
+      <div class="home_body"> 
+
+
+
+<div style="margin-left:" class = "loggedhome_body">
+
+
+<div style="margin-left:" class = "landing_home_body">
+   <ul>
+      <li><img src = "photos/post.jpg" style="border-radius: 50px;" width = "200px" height = "200px" class = "responsive"></li>
+      <li><img src = "photos/courses.jpg" width = "200px" height = "200px" class  = "responsive"></li>
+      <li><img src = "photos/reports.png" width = "200px" height = "200px" class = "responsive"></li>
+      <li><img src = "photos/blogs.jpg" style="border-radius: 50px;" width = "200px" height = "200px" class = "responsive"></li>
+
+   </ul>
+</div>
+
+<div style="margin-left:15px" class = "landing_home_text">
+   <ul>
+      <li><a href="createpost/postview.php" ><Button style="background-color:#33ca93">My Posts </Button></a></li>
+      <li><a href="posts" ><Button style="background-color:#33ca93">Courses </Button></a></li>
+      <li><a href="reports/agriD.php" ><Button style="background-color:#33ca93">Repots</Button></a></li>
+      <li><a href="blogs" ><Button style="background-color:#33ca93">Blogs </Button></a></li>
+   </ul>
+</div>
+
+</div>
+      </div>
+</body>
+<footer>
+<?php include 'include/footer.php' ?>
+</footer>
+</html>
