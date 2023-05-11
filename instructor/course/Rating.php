f7ec87e89e11e3fb299a1be75b5fd3ab684afbbc
Nadeesha142 <2020cs142@stu.ucsc.cmb.ac.lk>
2023-05-11 22:44:43 +0530

    complete course_forum

---
+++ instructor/course/Rating.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
+    <title>Course Ratings</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
