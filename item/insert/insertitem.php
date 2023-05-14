99c5f67b2c6b925416985f8f6b6a5e12daa24b67
oshaniwickramasinghe <84929391+oshaniwickramasinghe@users.noreply.github.com>
2023-05-14 23:11:22 +0530

    item exist modal

--- item/insert/insertitem.php
+++ item/insert/insertitem.php
@@ -1,9 +1,22 @@
-
-    <?php 
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../public/CSS/style.css">
+        <link rel="stylesheet" href="../../admin/notification.css">
+        <!-- <link rel="stylesheet" href="../../instructor/profile/InstructorProfile.css"> -->
+        <title>Admin view for profile</title>
+        
+        
+</head>
+    
+	
+	<?php 
 
         require "../../database/database.php";
         require "../../public/Auth.php";
-		include '../includes/header.php';
+		// include '../includes/header.php';
    
 
 		$item_name = $_REQUEST['item_name'];
@@ -14,9 +27,46 @@
 
 		$row=mysqli_query($conn, $query);
 
+		//call modal if item already exists
 		if(mysqli_num_rows($row)>0)
 		{
-			echo "Item Already Exists"; 
+
+			?>
+
+		<div class="container">
+			<div id="id01" class="modal_delete" style="display: none;">
+                <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+                
+                <div class="modal_content_delete" action="">
+                    <div class="container_delete">
+                        <h1>Item already exists</h1>
+                        <div class="clearfix_delete">
+                            <button type="button" class="cancelbtn" onclick="hideModal()">Go back</button>
+                        </div>
+                    </div>
+                </div>
+            </div>   
+		</div>
+
+		<script >
+
+			function showModal() {
+				document.getElementById("id01").style.display = "flex";
+				// retrieves the div element with the ID of id01 and sets its display style property to flex. This makes the modal box visible on the web page.
+			}
+			
+			function hideModal() {
+				document.getElementById("id01").style.display = "none";
+				window.location.href = "../view/itemview.php";
+				// retrieves the div element with the ID of id01 and sets its display style property to none. This makes the modal box hidden on the web page.
+			}
+		</script>
+
+			<?php
+
+			echo '<script>';
+			echo 'showModal();'; 
+			echo '</script>';
 			exit;
 		}
 		else
@@ -50,4 +100,6 @@
 		
 		// Close connection
 		mysqli_close($conn);
+
+		
 		?>
